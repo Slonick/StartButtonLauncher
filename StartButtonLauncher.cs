6cf0f2564a51f1f0b256b6a7286d80134eb88e6f
slonick <artem.kononenko@slonick.dev>
2025-10-05 01:04:08 +0300

    minor changes

--- StartButtonLauncher.cs
+++ StartButtonLauncher.cs
@@ -1,31 +1,32 @@
-﻿using Playnite.SDK;
-using Playnite.SDK.Events;
-using Playnite.SDK.Plugins;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Playnite.SDK;
+using Playnite.SDK.Events;
+using Playnite.SDK.Plugins;
 
 namespace StartButtonLauncher
 {
     public class StartButtonLauncher : GenericPlugin
     {
+        private const string PlayniteTitle = "Playnite.DesktopApp";
         private static readonly ILogger logger = LogManager.GetLogger();
         private CancellationTokenSource cts;
+        private bool wasStartPressed;
+
+        public StartButtonLauncher(IPlayniteAPI playniteAPI) : base(playniteAPI) { }
 
         public override Guid Id => Guid.Parse("80e02e7f-57cd-43f0-b501-45d5ce16f10f");
-
-        public StartButtonLauncher(IPlayniteAPI playniteAPI) : base(playniteAPI) 
-        {
-        }
 
         public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
         {
             logger.Info("StartButtonPlugin started.");
             this.cts = new CancellationTokenSource();
-            Task.Run(() => PollGamepad(cts.Token));
+            Task.Run(() => this.PollGamepad(this.cts.Token));
         }
 
         public override void OnApplicationStopped(OnApplicationStoppedEventArgs args)
@@ -33,59 +34,74 @@ namespace StartButtonLauncher
             this.cts?.Cancel();
         }
 
-        private async Task PollGamepad(CancellationToken token)
-        {
-            while (!token.IsCancellationRequested)
-            {
-                
-                if (XInput.GetState(0, out var state))
-                {
-                    bool isStartPressed = state.Gamepad.wButtons.HasFlag(XInputButtons.Start);
-
-                    if (isStartPressed && this.PlayniteApi.ApplicationInfo.Mode != ApplicationMode.Fullscreen)
-                    {
-                        if (PlayniteApi.Database.Games.All(g => g.IsRunning == false && g.IsRunning == false))
-                        {
-                            this.LaunchFullscreen();
-                        }
-                    }
-                }
-
-                await Task.Delay(100, token);
-            }
-        }
-
-        private void LaunchFullscreen()
+        private void LaunchPlaynite(bool fullscreen)
         {
             try
             {
-                var fullscreenExe = Path.Combine(PlayniteApi.Paths.ApplicationPath, "Playnite.FullscreenApp.exe");
+                var fullscreenExe = Path.Combine(this.PlayniteApi.Paths.ApplicationPath, "Playnite.DesktopApp.exe");
 
                 if (!File.Exists(fullscreenExe))
                 {
-                    PlayniteApi.Notifications.Add(new NotificationMessage(
-                        "fullscreen_not_found",
-                        $"Fullscreen executable not found: {fullscreenExe}",
-                        NotificationType.Error));
+                    this.PlayniteApi.Notifications.Add(new NotificationMessage(
+                                                                               "executable_not_found",
+                                                                               $"Executable not found: {fullscreenExe}",
+                                                                               NotificationType.Error));
                     return;
+                }
+
+                var argumanets = new List<string> { "--hidesplashscreen" };
+
+                if (fullscreen)
+                {
+                    argumanets.Add("--startfullscreen");
                 }
 
                 var psi = new ProcessStartInfo
                 {
                     FileName = fullscreenExe,
-                    Arguments = "--startfullscreen --hidesplashscreen",
+                    Arguments = string.Join(" ", argumanets),
                     UseShellExecute = false,
-                    WorkingDirectory = PlayniteApi.Paths.ApplicationPath
+                    WorkingDirectory = this.PlayniteApi.Paths.ApplicationPath
                 };
 
                 Process.Start(psi);
             }
             catch (Exception ex)
             {
-                PlayniteApi.Notifications.Add(new NotificationMessage(
-                    "fullscreen_launch_error",
-                    $"Error launching fullscreen: {ex.Message}",
-                    NotificationType.Error));
+                this.PlayniteApi.Notifications.Add(new NotificationMessage(
+                                                                           "executable_launch_error",
+                                                                           $"Error launching executable: {ex.Message}",
+                                                                           NotificationType.Error));
+            }
+        }
+
+        private async Task PollGamepad(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if (XInput.GetState(0, out var state))
+                {
+                    var isStartPressed = state.Gamepad.wButtons.HasFlag(XInputButtons.Start);
+
+                    if (isStartPressed && !this.wasStartPressed && this.PlayniteApi.ApplicationInfo.Mode != ApplicationMode.Fullscreen)
+                    {
+                        if (this.PlayniteApi.Database.Games.All(g => !g.IsRunning && !g.IsLaunching))
+                        {
+                            if (WindowHelper.IsWindowVisible(PlayniteTitle))
+                            {
+                                this.LaunchPlaynite(true);
+                            }
+                            else
+                            {
+                                WindowHelper.ShowWindow(PlayniteTitle);
+                            }
+                        }
+                    }
+
+                    this.wasStartPressed = isStartPressed;
+                }
+
+                await Task.Delay(100, token).ConfigureAwait(false);
             }
         }
     }
---
+++ WindowHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace StartButtonLauncher
+{
+    public static class WindowHelper
+    {
+        private const int SW_RESTORE = 9;
+
+        public static bool IsWindowVisible(string title)
+        {
+            var process = Process.GetProcessesByName(title).FirstOrDefault();
+            if (process != null && process.MainWindowHandle != IntPtr.Zero)
+            {
+                return IsWindowVisible(process.MainWindowHandle);
+            }
+
+            return false;
+        }
+
+        public static void ShowWindow(string title)
+        {
+            var process = Process.GetProcessesByName(title).FirstOrDefault();
+            ShowWindow(process.MainWindowHandle, SW_RESTORE);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+}
